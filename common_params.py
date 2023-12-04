4b45fad7fd0161c8d35566ebecf82b941d472fb3
David Perkel <perkel@uw.edu>
2023-12-03 22:40:40 -0800

    Minor updates supporting development of other files

--- common_params.py
+++ common_params.py
@@ -17,7 +17,7 @@ STD_TEXT = 'STDR' + str(ACT_STDREL)
 STD_TEXT = STD_TEXT.replace('.', '_')
 
 # File locations
-sigmaVals = [0.5, 0.9]  # Always explore monopolar stimulation and one value of sigma for triploar
+sigmaVals = [0, 0.9]  # Always explore monopolar stimulation and one value of sigma for triploar
 # can be overridden for individual subjects
 
 COCHLEA = {'source': 'manual', 'timestamp': [], 'radius': []}
@@ -64,7 +64,7 @@ ct_uncertainty = 0.1  # uncertainty for CT values in case one wants to display i
 # scenarios = ['RampRposRampSurv']
 # scenarios = ['ExtremeHole']
 # scenarios = ['RampRposS80']
-# scenarios = ['RampRposSGradual80']
+scenarios = ['RampRposSGradual80']
 # scenarios = ['Checking_REXT_2500']
 # scenarios = ['CustomForECAPFigure']
 # scenarios = ['Step40_80R00']
@@ -74,7 +74,8 @@ ct_uncertainty = 0.1  # uncertainty for CT values in case one wants to display i
 # Actual subject data. For inverse model only
 # scenarios = ['S40', 'S42']  # paper "good fit" examples. Figure 7
 # scenarios = ['S29', 'S56']  # paper "poor fit" examples. Figure 8
-scenarios = ['A002R', 'A014L', 'A022L', 'A022R', 'A023R', 'A024L']
+# scenarios = ['A002R', 'A014L', 'A022L', 'A022R', 'A023R', 'A024L']
+# scenarios = ['A014L']
 # all subjects with CT data
 # scenarios = ['S22', 'S27', 'S29', 'S38', 'S40', 'S41', 'S42', 'S43', 'S46', 'S47', 'S49R', 'S50', 'S52', 'S53', 'S54',
 #              'S55', 'S56', 'S57']
